f2203549332a8e39fdc6d11c372b6ec366617307
Vitalii Kravets <vitalii.kravets@gmail.com>
2015-12-15 22:37:22 +0100

    Fixing heroku env

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,7 +4,7 @@
 # --- !Ups
 
 create table books (
-  id                        bigint not null,
+  id                        bigint not null auto_increment,
   inserted_at               timestamp not null,
   external_id               varchar(255) not null,
   title                     varchar(255) not null,
@@ -13,18 +13,20 @@ create table books (
   thumbnail_url             TEXT not null,
   small_thumbnail_url       TEXT not null,
   constraint pk_books primary key (id))
+  engine=innodb
 ;
 
 create table users (
-  id                        bigint not null,
+  id                        bigint not null auto_increment,
   inserted_at               timestamp not null,
   username                  varchar(255) not null,
   constraint uq_users_username unique (username),
   constraint pk_users primary key (id))
+  engine=innodb
 ;
 
 create table users_books (
-  id                        bigint not null,
+  id                        bigint not null auto_increment,
   inserted_at               timestamp not null,
   user_id                   bigint not null,
   book_id                   bigint not null,
@@ -32,13 +34,8 @@ create table users_books (
   constraint ck_users_books_type check (type in ('READ','PLANNING_TO_READ')),
   constraint uq_users_books_1 unique (user_id,book_id),
   constraint pk_users_books primary key (id))
+  engine=innodb
 ;
-
-create sequence books_seq;
-
-create sequence users_seq;
-
-create sequence users_books_seq;
 
 alter table users_books add constraint fk_users_books_user_1 foreign key (user_id) references users (id) on delete restrict on update restrict;
 create index ix_users_books_user_1 on users_books (user_id);
@@ -49,7 +46,7 @@ create index ix_users_books_book_2 on users_books (book_id);
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+SET FOREIGN_KEY_CHECKS = 0
 
 drop table if exists books;
 
@@ -57,7 +54,7 @@ drop table if exists users;
 
 drop table if exists users_books;
 
-SET REFERENTIAL_INTEGRITY TRUE;
+SET FOREIGN_KEY_CHECKS = 1
 
 drop sequence if exists books_seq;
 
